34e2801ed711256c6700ed42866ee8545f8796be
tbahrit.m <m.tbahriti@esi-sba.dz>
2025-02-19 19:30:03 +0100

    Initial Commit

---
+++ datav2Tests/datav2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  datav2Tests.swift
+//  datav2Tests
+//
+//  Created by Mohammed TBAHRITI on 19/02/2025.
+//
+
+import Testing
+@testable import datav2
+
+struct datav2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ datav2UITests/datav2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  datav2UITestsLaunchTests.swift
+//  datav2UITests
+//
+//  Created by Mohammed TBAHRITI on 19/02/2025.
+//
+
+import XCTest
+
+final class datav2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
